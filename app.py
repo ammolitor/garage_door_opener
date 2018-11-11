677f556e33c4dc5665ac6f48e8c4f17122cecac2
Aaron Molitor <ammolitor@gmail.com>
2018-11-10 19:53:27 -0600

    more updates, trying to not stay on /button after click

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-'''
+"""
     Raspberry Pi GPIO Status and Control
-'''
+"""
 import time
 import RPi.GPIO as GPIO
-from flask import Flask, render_template
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
 def push_button():
@@ -18,26 +18,23 @@ def push_button():
 
 
 @app.route("/")
+@app.route("/index")
 def index():
-    templateData = {
+    template_data = {
         'title': 'Garage Door'
     }
-    return render_template('index.html', **templateData)
+    return flask.render_template('index.html', **template_data)
 
 
 @app.route("/button")
 def push_button():
-    templateData = {
-        'title': 'Garage Door',
-        'command': 'SUCCESS'
-    }
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(7, GPIO.OUT)
     GPIO.output(7, GPIO.LOW)
     time.sleep(0.5)
     GPIO.output(7, GPIO.HIGH)
     GPIO.cleanup()
-    return render_template('index.html', **templateData)
+    return flask.redirect(flask.url_for('index'))
 
 
 if __name__ == "__main__":
